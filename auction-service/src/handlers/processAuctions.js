96dc967f6ed0d9c1552e4415e469fd86a1c14fb6
Jorge Souza <regi.jorge.s@gmail.com>
2020-07-05 12:52:31 -0300

    Closing auctions

--- auction-service/src/handlers/processAuctions.js
+++ auction-service/src/handlers/processAuctions.js
@@ -1,7 +1,18 @@
 import { getEndedAuctions } from '../lib/getEndedAuctions'
+import { closeAuction } from '../lib/closeAuction'
+import createError from 'http-errors'
 
 async function processAuctions(event, context) {
-  const auctionsToBeClosed = await getEndedAuctions()
+  try {
+    const auctionsToBeClosed = await getEndedAuctions()
+    const closePromises = auctionsToBeClosed.map(auction => closeAuction(auction.id))
+    await Promise.all(closePromises)
+
+    return { closed: closePromises.length }
+  } catch (error) {
+    console.log(error)
+    throw createError.InternalServerError(error)
+  }
 }
 
 export const handler = processAuctions
