c993286541b00ae28e2c81e70354b2c68916f276
Jorge Souza <regi.jorge.s@gmail.com>
2020-07-05 16:59:38 -0300

    Sending email test

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,8 +1,32 @@
+import AWS from 'aws-sdk'
+
+const ses = new AWS.SES({ region: 'us-east-1' })
+
 async function sendMail(event, context) {
-  console.log(event)
-  return event
+  const params = {
+    Source: 'a@b.com',
+    Destination: {
+      ToAddresses: ['a@b.com']
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello!'
+        }
+      },
+      Subject: {
+        Data: 'Test Mail'
+      }
+    }
+  }
+
+  try {
+    const result = await ses.sendEmail(params).promise()
+    console.log(result)
+    return result
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export const handler = sendMail
-
-
