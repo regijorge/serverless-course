aeeb2507e00b38110fed2a35c5ade2f61135d0c4
Jorge Souza <regi.jorge.s@gmail.com>
2020-07-06 21:25:11 -0300

    Uploading pictures to S3

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,62 +1,27 @@
-import { v4 as uuid } from 'uuid'
-import AWS from 'aws-sdk'
-import commonMiddleware from '../lib/commonMiddleware'
-import createError from 'http-errors'
 import { getAuctionById } from './getAuction'
-import validator from '@middy/validator'
-import schema from '../lib/schemas/placeBidSchema'
-
-const dynamodb = new AWS.DynamoDB.DocumentClient()
+import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
+import middy from '@middy/core'
+import httpErrorHandler from '@middy/http-error-handler'
 
 async function uploadAuctionPicture(event, context) {
   const { id } = event.pathParameters
-  const { email } = event.requestContext.authorizer
-  const { amount } = event.body
-
-  AUCTIONS_BUCKET_NAME
 
   const auction = await getAuctionById(id)
+  const base64 = event.body.replace(/^data:image\/\w+base64,/, '')
+  const buffer = Buffer.from(base64, 'base64')
 
-  if (email <= auction.seller) {
-    throw createError.Forbidden('You can not bid on your own auction')
-  }
-
-  if (email <= auction.highestBid.bidder) {
-    throw createError.Forbidden('You are already the highest bidder')
-  }
-
-  if (auction.status !== 'OPEN') {
-    throw createError.Forbidden('You can not bid on closed auctions')
-  }
-
-  if (amount <= auction.highestBid.amount) {
-    throw createError.Forbidden('Amoount must be higher')
-  }
-
-  const params = {
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
-    ExpressionAttributeValues: {
-      ':amount': amount,
-      ':bidder': email
-    },
-    ReturnValues: 'ALL_NEW'
-  }
-
-  let upadtedAuction
   try {
-    const result = await dynamodb.update(params).promise()
-    upadtedAuction = result.Attributes
+    const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer)
+    console.log(uploadToS3Result)
   } catch (error) {
     console.log(error)
-    throw createError.InternalServerError(error)
   }
 
   return {
-    statusCode: 201,
-    body: JSON.stringify(upadtedAuction),
+    statusCode: 200,
+    body: JSON.stringify({})
   }
 }
 
-export const handler = uploadAuctionPicture
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler)
