d5b42c0e825173143ddcc8d75ecde6ca81382dee
Jorge Souza <regi.jorge.s@gmail.com>
2020-07-06 21:43:39 -0300

    Saving picture urls in DynamoDB

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,5 +1,6 @@
 import { getAuctionById } from './getAuction'
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
+import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl'
 import middy from '@middy/core'
 import httpErrorHandler from '@middy/http-error-handler'
 
@@ -10,18 +11,19 @@ async function uploadAuctionPicture(event, context) {
   const base64 = event.body.replace(/^data:image\/\w+base64,/, '')
   const buffer = Buffer.from(base64, 'base64')
 
+  let updatedAuction
   try {
-    const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer)
-    console.log(uploadToS3Result)
+    const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer)
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl)
   } catch (error) {
     console.log(error)
   }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({})
+    body: JSON.stringify(updatedAuction)
   }
 }
 
 export const handler = middy(uploadAuctionPicture)
-  .use(httpErrorHandler)
+  .use(httpErrorHandler())
